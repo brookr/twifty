ef56c9542c243290222d6822180c26208e09c48c
Brook Riggio <brooke.riggio@gmail.com>
2013-09-11 15:22:00 -0700

    Tweet queue example Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twifty::Application.config.secret_token = '9cabfa8c928614d191fc06f367c29858ffad1c186771fc3886ae0dddf50261458dec50292437e6391e76f5ba499c39d7764519624c7b6a92fe0a0bb04a43deed'
